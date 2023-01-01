63e2ba3ec03f5b6cd6b3e7a50aaa81d168c465e2
Tomas Balaz <balaztomas0@gmail.com>
2023-01-01 14:44:22 +0100

    Create spring-boot-3-config-properties application

---
+++ src/main/java/sk/balaz/springboot3configproperties/SpringBoot3ConfigPropertiesApplication.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboot3configproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3ConfigPropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3ConfigPropertiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sk/balaz/springboot3configproperties/SpringBoot3ConfigPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.balaz.springboot3configproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3ConfigPropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
