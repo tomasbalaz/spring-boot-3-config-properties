e325318312b2fc97b0e195f52a49e1267ae2f3e1
Tomas Balaz <balaztomas0@gmail.com>
2023-01-02 15:52:50 +0100

    Binding properties to Immutable Objects using constructor binding

--- src/main/java/sk/balaz/springboot3configproperties/config/ApplicationProperties.java
+++ src/main/java/sk/balaz/springboot3configproperties/config/ApplicationProperties.java
@@ -9,78 +9,62 @@ import org.springframework.validation.annotation.Validated;
 @Validated
 public class ApplicationProperties {
 
-    private String version;
+    private final String version;
 
-    private FtpProperties ftp;
+    private final FtpProperties ftp;
+
+    public ApplicationProperties(String version, FtpProperties ftp) {
+        this.version = version;
+        this.ftp = ftp;
+    }
 
     public String getVersion() {
         return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
     }
 
     public FtpProperties getFtp() {
         return ftp;
     }
 
-    public void setFtp(FtpProperties ftp) {
-        this.ftp = ftp;
-    }
+    public record FtpProperties(@NotEmpty String host, @Positive String port, @NotEmpty String username,
+                                @NotEmpty String password) {
+            public FtpProperties(String host, String port, String username, String password) {
+                this.host = host;
+                this.port = port;
+                this.username = username;
+                this.password = password;
+            }
 
-    public static class FtpProperties {
-        @NotEmpty
-        private String host;
-        @Positive
-        private String port;
-        @NotEmpty
-        private String username;
-        @NotEmpty
-        private String password;
+            @Override
+            public String host() {
+                return host;
+            }
 
-        public String getHost() {
-            return host;
+            @Override
+            public String port() {
+                return port;
+            }
+
+            @Override
+            public String username() {
+                return username;
+            }
+
+            @Override
+            public String password() {
+                return password;
+            }
+
+            @Override
+            public String toString() {
+                return "FtpProperties{" +
+                        "host='" + host + '\'' +
+                        ", port='" + port + '\'' +
+                        ", username='" + username + '\'' +
+                        ", password='" + password + '\'' +
+                        '}';
+            }
         }
-
-        public void setHost(String host) {
-            this.host = host;
-        }
-
-        public String getPort() {
-            return port;
-        }
-
-        public void setPort(String port) {
-            this.port = port;
-        }
-
-        public String getUsername() {
-            return username;
-        }
-
-        public void setUsername(String username) {
-            this.username = username;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        @Override
-        public String toString() {
-            return "FtpProperties{" +
-                    "host='" + host + '\'' +
-                    ", port='" + port + '\'' +
-                    ", username='" + username + '\'' +
-                    ", password='" + password + '\'' +
-                    '}';
-        }
-    }
 
     @Override
     public String toString() {
